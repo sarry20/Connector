a21dc43f19991280c9fea7657b05e10b459f55f0
sarry20 <izansr67@gmail.com>
2024-01-02 01:27:23 +0100

    packetevents impl #6

--- src/main/java/com/sarry20/Sender.java
+++ src/main/java/com/sarry20/Sender.java
@@ -4,24 +4,36 @@ import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.netty.buffer.ByteBufHelper;
+import com.github.retrooper.packetevents.protocol.ConnectionState;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.configuration.client.WrapperConfigClientKeepAlive;
+import com.github.retrooper.packetevents.wrapper.configuration.server.WrapperConfigServerKeepAlive;
+import com.github.retrooper.packetevents.wrapper.login.client.WrapperLoginClientLoginSuccessAck;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientKeepAlive;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerKeepAlive;
 
 public class Sender implements PacketListener {
     @Override
     public void onPacketSend(PacketSendEvent event) {
-        System.out.println(event);
+        System.out.println("PACKET EVENT: "+event.getPacketName());
         if (event.getPacketType() == PacketType.Play.Client.KEEP_ALIVE){
             WrapperPlayServerKeepAlive keepAlive = new WrapperPlayServerKeepAlive(event);
             WrapperPlayClientKeepAlive alive = new WrapperPlayClientKeepAlive(keepAlive.getId());
+            event.getUser().sendPacket(alive);
+        }else if(event.getPacketType() == PacketType.Login.Server.LOGIN_SUCCESS){
+            WrapperLoginClientLoginSuccessAck ack = new WrapperLoginClientLoginSuccessAck();
+            event.getUser().sendPacket(ack);
+            event.getUser().setConnectionState(ConnectionState.CONFIGURATION);
+        }else if(event.getPacketType() == PacketType.Configuration.Server.CONFIGURATION_END){
+            event.getUser().setConnectionState(ConnectionState.PLAY);
+        }else if(event.getPacketType() == PacketType.Configuration.Server.KEEP_ALIVE){
+            WrapperConfigServerKeepAlive keepAlive = new WrapperConfigServerKeepAlive(event);
+            WrapperConfigClientKeepAlive alive = new WrapperConfigClientKeepAlive(keepAlive.getId());
             event.getUser().sendPacket(alive);
         }
     }
 //    @Override
 //    public void onPacketLoginReceive(PacketLoginReceiveEvent event) {
-//        System.out.println(event);
-//        System.out.println(event.getPacketType());
 //        if (event.getPacketType() == PacketType.Login.Server.ENCRYPTION_REQUEST){
 //            try {
 //                WrapperLoginServerEncryptionRequest request = new WrapperLoginServerEncryptionRequest(event);
